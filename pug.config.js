af0f533c54edcdc3bf6809b71fbfc15a63604a5f
William04A <35110380+William04A@users.noreply.github.com>
2023-06-09 19:26:06 +0200

    Update image URLs to use backup website instead

--- pug.config.js
+++ pug.config.js
@@ -24,7 +24,7 @@ module.exports = {
             "about": "Denna matsvamp är en fyrstjärnig sopp och tillhör en av de godare sopparna man kan hitta. Den har också ett karaktäristiskt utseende. Det kan dock ändå vara lite knepigt att hitta ett fint exemplar. Svampen är nämligen som godast när den inte vuxit så längre. Som de flesta soppar tenderar denna att smaka sämre och ha fått påhälsning av kryp och maskar i takt med dess ålder.",
             "appearance": "Svampen kännetecknas av dess smörfärgade rör på undersidan. I regnig väderlek blir svampen även glansig på hatten. Yngre exemplar har en vit ring runt foten och kan ha en hinna utanför rören. Denna bör man ta bort.",
             "location": "Svampen kan hittas över hela Sverige men växer i symbios med tall. Då den blir hal och glansig på hatten är det ultimat att plocka den när det är torrt ute, men om man hittar den ute i skogen en regnig dag får man självklart inte missa den.",
-            "image": "https://20alse.ssis.nu/svampbilder/smorsopp.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/smorsopp.jpg",
             "imageLicense": {
               "type": "CC BY-SA 3.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
@@ -53,7 +53,7 @@ module.exports = {
             "about": "Vi har nog alla hört namnet oavsett våra erfarenheter med svamp. Detta är en femstjärning sopp som är mycket smarrig och har en mild smak med nötiga toner. Svampen är även känd under namnet stensopp.",
             "appearance": "Foten på svampen är ljusbrun eller vit och knubbig. Hatten kan vara både ljusbrun och mörkbrun. På yngre svampar är rören vita och på äldre är de gula/gulbruna.",
             "location": "Enligt Artportalen hittar man främst denna svamp i södra Sverige, men den förekommer också på nordligare ställen.",
-            "image": "https://20alse.ssis.nu/svampbilder/karljohan.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/karljohan.jpg",
             "imageLicense": {
               "type": "CC0 1.0",
               "sourceURL": "https://www.flickr.com/photos/volvob12b/16424322871",
@@ -72,7 +72,7 @@ module.exports = {
             "about": "En trestjärning matsvamp. Äldre exemplar är inte lika goda som yngre.",
             "appearance": "Namnet av svampen ger en ledtråd om dess utseende. Foten liknar en björk.",
             "location": "Förekommer över hela landet.",
-            "image": "https://20alse.ssis.nu/svampbilder/bjorksopp.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/bjorksopp.jpg",
             "imageLicense": {
               "type": "CC BY 4.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
@@ -92,7 +92,7 @@ module.exports = {
             "about": "Som man hör på namnet så växer denna sopp tillsammans med asp. Den tillhör strävsopparna.",
             "appearance": "Hatten är rödbrun eller orange. Rören är vita. Om man skär svampen är snitten rodnande, men sedan övergår de till att bli svartare.",
             "location": "Förekommer över hela landet.",
-            "image": "https://20alse.ssis.nu/svampbilder/aspsopp.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/aspsopp.jpg",
             "imageLicense": {
               "type": "CC BY-SA 4.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
@@ -112,7 +112,7 @@ module.exports = {
             "about": "Tegelsopp är enligt Svampguiden en av de mer fastare strävsopparna. Svampen är mild och passar likt många andra strävsoppar därför bra att ha tillsammans med andra svampar.",
             "appearance": "Hatten är rödbrun eller orange. Rören är vita. Om man skär svampen är snitten rodnande, men sedan övergår de till att bli svartare.",
             "location": "Förekommer över hela landet.",
-            "image": "https://20alse.ssis.nu/svampbilder/tegelsopp.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/tegelsopp.jpg",
             "imageLicense": {
               "type": "CC BY 2.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
@@ -148,7 +148,7 @@ module.exports = {
             "about": "Kantarellen hör till en våra allra mest kända svampar och har fått smeknamnet \"skogens guld\" av en anledning. Dens popularitet beror på flera faktorer, däribland att den är lätt att känna igen, att den är mångsidig i matlagning, att den ytterst sällan är maskangripen, och sist att det finns få saker som slår smörstekta kantareller.",
             "appearance": "Svampen är svår att förväxla och karaktäriseras av dess guldskimrande utsida med rör i samma färg som svampen. Hatten kan ibland vara veckad och kantareller kommer i olika former och storlekar. Den enda förväxlingssvampen kallas \"falsk kantarell\" och skiljer sig en hel del från kantarellens utseende. Sök online för att hitta en bild om du är osäker.",
             "location": "Arten förekommer i löv- och barrskogar i hela Sverige, däremot begränsat i Norrlands inland och fjällen.",
-            "image": "https://20alse.ssis.nu/svampbilder/kantareller.png",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/kantareller.png",
             "monthData": {
               "onTheWayIn": 6,
               "primeTime": [7, 8, 9],
@@ -167,7 +167,7 @@ module.exports = {
             "about": "Svart trumpetsvamp är en riktigt god svamp och i mitt tycke är den underskattad. Detta kanske beror på att den är ytterst sällsynt.",
             "appearance": "Svart trumpetsvamp har en trattformad hatt med tunt kött.",
             "location": "Denna svamp hittar man ofta dold bland mossa i löv- och blandskogar. Den förekommer (men är mer sällsynt) i norra delar av Sverige.",
-            "image": "https://20alse.ssis.nu/svampbilder/svart_trumpetsvamp.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/svart_trumpetsvamp.jpg",
             "imageLicense": {
               "type": "CC BY-SA 4.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
@@ -187,7 +187,7 @@ module.exports = {
             "about": "Här har vi en riktigt smarrig, fyrstjärning matsvamp som dessutom inte har några giftiga förväxlingssvampar om du inte är riktigt slarvig. Ofta växer flera trattkantareller nära varandra, så se till att titta efter ordentligt om du hittar en!",
             "appearance": "Hatten är tunn och brun eller ljusbrun och foten är gul. Hatten har skivor som når ner en bit på foten.",
             "location": "Förekommer över hela landet och trivs i mossig barrskog.",
-            "image": "https://20alse.ssis.nu/svampbilder/trattkantareller.png",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/trattkantareller.png",
             "monthData": {
               "onTheWayIn": 9,
               "primeTime": 10,
@@ -213,7 +213,7 @@ module.exports = {
             "about": "En femstjärning och otroligt smarrig men relativt sällsynt. Har inga farliga förväxlingssvampar.",
             "appearance": "Blek färg samt små \"taggar\" under hatten. Trillar lätt sönder vid tryck.",
             "location": "Förekommer i många olika skogstyper i Sverige.",
-            "image": "https://20alse.ssis.nu/svampbilder/blek_taggsvamp.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/blek_taggsvamp.jpg",
             "imageLicense": {
               "type": "CC BY-SA 3.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
@@ -233,7 +233,7 @@ module.exports = {
             "about": "En enstjärning matsvamp som till skillnad från sin kollega, blek taggsvamp, inte är ansedd som någon god matsvamp. Den är däremot ätlig, men endast som ung. Som äldre är den inte giftig, men oaptitlig.",
             "appearance": "Som namnet antyder påminner färgen på hatten om bröd. Undersidan samt foten är vit och kan vara orangefläckig.",
             "location": "",
-            "image": "https://20alse.ssis.nu/svampbilder/brodticka.jpg",
+            "image": "https://20alse.albins.website/start_page_assets/svampbilder/brodticka.jpg",
             "imageLicense": {
               "type": "CC BY-SA 4.0",
               "extraText": "Inga modifikationer av originalbilden har gjorts förutom att filstorleken har komprimerats.",
